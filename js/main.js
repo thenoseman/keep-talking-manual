1b65e2f5436d334a88293d2f297f995ea9636479
Frank Schumacher <frank.schumacher.privat@gmail.com>
2015-12-08 09:39:29 +0100

    mark important fields in needy knob

--- js/main.js
+++ js/main.js
@@ -1,25 +1,39 @@
 $(document).ready(function () {
-    // Fill in page footers:
-    var pageCount = $(".page").size();
-    pageCount -= $(".no-page-count").size();
-    var currentPage = 0;
-	var currentBG = 1;
-    $(".page").each(function (index) {
-        if ($(this).hasClass("no-page-count") == false) {
-            currentPage++;
-            $(".page-footer", $(this)).each(function (index) {
-                if ($(this).hasClass("no-auto-footer") == false) {
-                    $(this).html("Page " + currentPage + " of " + pageCount);
-                }
-            });
-
-			// pages without page count are the main body
-			$(this).addClass('page-bg-0' + currentBG);
-			currentBG++
-			if(currentBG > 7)
-			{
-				currentBG = 1;
-			}
+  // Fill in page footers:
+  var pageCount = $(".page").size();
+  pageCount -= $(".no-page-count").size();
+  var currentPage = 0;
+  var currentBG = 1;
+  $(".page").each(function (index) {
+    if ($(this).hasClass("no-page-count") == false) {
+      currentPage++;
+      $(".page-footer", $(this)).each(function (index) {
+        if ($(this).hasClass("no-auto-footer") == false) {
+          $(this).html("Page " + currentPage + " of " + pageCount);
         }
+      });
+
+      // pages without page count are the main body
+      $(this).addClass('page-bg-0' + currentBG);
+      currentBG++
+        if(currentBG > 7)
+        {
+          currentBG = 1;
+        }
+    }
+  });
+
+  $(".needy-knob table")
+    .find("td:nth-child(5)")
+    .addClass("imp")
+    .end()
+    .find("tr:nth-child(2)").each(function(i2, e) {
+      console.log(e);
+      $(e).find("td").each(function(index, f) {
+        if(index > 2) {
+          $(this).addClass("imp");
+        }
+      });
     });
+
 });
